fcc9eadca3bc095c4d6d70a171748c11827d79a7
NerlyTon <nerlyton@gmail.com>
2020-08-31 21:19:30 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
     end
 
     def self.most_popular_show
-        Show.find.highest_rating
+        Show.order(rating: :desc).take
     end
 
     def self.lowest_rating
