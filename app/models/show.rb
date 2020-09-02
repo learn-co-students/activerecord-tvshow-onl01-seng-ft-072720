03e358ce50806edd305c21d497d2210d2ee9591b
popkorn96 <mpaloma2014@gmail.com>
2020-09-02 13:25:49 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,27 +1,33 @@
 class Show < ActiveRecord::Base
-    def highest_rating
+    def self.highest_rating
         # this method should return the highest value in the ratings column. 
         #hint: if there is a minimum Active Record method, might there be a maximum method?
-        self.ratings.maximum
+        Show.maximum(:rating)
+    end
+    
+    def self.most_popular_show
+        movie = Show.where(:rating == highest_rating)
+        movie.take
+
+    end
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+    def self.least_popular_show
+        self.find_by(rating: lowest_rating)
+    end
+    def self.ratings_sum
+        counts = Show.sum(:rating)
+        counts
+    end
+    def self.popular_shows
+        movies = Show.where('rating > 5')
+        movies
+    end
+    def self.shows_by_alphabetical_order
+        orders = Show.order(:name) 
+        orders
     end
 
-    def most_popular_show
-        #
-    end
-    def lowest_rating
-        #
-    end
-    def least_popular_show
-        #
-    end
-    def ratings_sum
-        #
-    end
-    def popular_shows
-        #
-    end
-    def shows_by_alphabetical_order
-        #
-    end
-
+    
 end
