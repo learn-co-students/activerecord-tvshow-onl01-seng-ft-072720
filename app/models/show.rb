17c2cc368a2a150b6b41ac47bf8d858758c1ca59
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-09-10 18:48:24 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,22 @@
 class Show < ActiveRecord::Base
-
-  def self.highest_rating
-      self.maximum("rating")
-  end
-
-  def self.most_popular_show
-      self.find_by(rating: self.highest_rating)
-  end
-
-  def self.lowest_rating
-      self.minimum('rating')
-  end
-
-  def self.least_popular_show
-      self.find_by(rating: self.lowest_rating)
-  end
-
-  def self.ratings_sum
-      self.sum(:rating)
-  end
-
-  def self.popular_shows
+    def self.highest_rating
+      self.all.maximum("rating")
+    end
+    def self.most_popular_show
+      self.where(:rating => highest_rating).first
+    end
+    def self.lowest_rating
+       self.all.minimum("rating")
+    end
+    def self.least_popular_show
+      self.where(:rating => lowest_rating).first
+    end
+    def self.ratings_sum
+      self.all.sum("rating")
+    end
+    def self.popular_shows
       self.where("rating > 5")
-  end
-
-  def self.shows_by_alphabetical_order
-      self.order(:name)
-  end
-end 
+    end
+    def self.shows_by_alphabetical_order
+       Show.all.order(:name)
+    end
